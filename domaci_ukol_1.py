cf5bdadbe4b063ab3d2e1617574a1d6f028bd717
TerezaMark <machova.ter@gmail.com>
2024-04-20 20:02:10 +0200

    Domácí úkol č. 1.

---
+++ domaci_ukol_1.py
@@ -0,0 +1,69 @@
+from dataclasses import dataclass
+
+@dataclass
+class Item:
+    name: str
+    price: float
+
+    def __str__(self):
+        return f"{self.name}: {self.price} Kč."
+    
+
+@dataclass
+class Pizza(Item):
+    ingredients: dict
+
+    def add_extra(self, ingredient: str, quantity: int, price_per_ingredient: int):
+            self.ingredients[ingredient] = quantity
+
+            extra_price = quantity*price_per_ingredient
+            self.price += extra_price
+            
+    def __str__(self):
+         return f"Pizza {self.name} s ingrediencemi {self.ingredients} stojí {self.price} Kč."
+
+
+@dataclass
+class Drink(Item):
+     volume: int
+
+     def __str__(self):
+          return f"Nápoj {self.name} o objemu {self.volume} ml stojí {self.price} Kč."
+     
+
+@dataclass
+class Order:
+     customer_name: str
+     delivery_address: str
+     items: list
+     status: str = "nová"
+
+     def mark_delivered(self):
+        self.status = "Doručeno"
+
+     def __str__(self):
+          return f"Zákazník {self.customer_name} objednal na adresu {self.delivery_address} tyto položky: {self.items}. Stav objednávky: {self.status}."
+     
+
+@dataclass
+class DeliveryPerson:
+    name: str
+    phone_number: str
+    available: bool = True
+    current_order: Order = None
+
+    def assign_order(self, order):
+        if self.available == True:
+            self.current_order = order
+            self.current_order.status = "Na cestě"
+            self.available = False
+        else:
+            print("Doručovatel má přiřazenou jinou objednávku.")
+
+    def complete_delivery(self):
+        self.current_order.status = "Doručeno"
+        self.available = True
+
+    def __str__(self):
+        return f"Doručovatel {self.name}, tel. číslo {self.phone_number}, dostupnost: {self.available}, objednávka: {self.current_order}."
+        
